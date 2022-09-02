235729cc5efed01e1a0d2561ec5ef6bf712a695a
Yusuf Ertürk <yusuf_erturk98@hotmail.com>
2022-09-02 21:50:11 +0300

    Add project files.

---
+++ src/kodlamaDevs/WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Models;
+using Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage;
+using Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage;
+using Core.Application.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListProgrammingLanguage([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingLanguageQuery getListProgrammingLanguageQuery = new() { PageRequest = pageRequest };
+            ProgrammingLanguageListModel result = await Mediator.Send(getListProgrammingLanguageQuery);
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetByIdProgrammingLanguage([FromRoute] GetByIdProgrammingLanguageQuery getByIdProgrammingLanguageQuery)
+        {
+            ProgrammingLanguageGetByIdDto result = await Mediator.Send(getByIdProgrammingLanguageQuery);
+            return Ok(result);
+        }
+    }
+}
